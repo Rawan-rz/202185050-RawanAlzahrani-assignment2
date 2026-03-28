c7c27ebbd4ec6f8ebfeafaec4b7b1adf9f717ef9
Rawan20000 <rawan_rz@icloud.com>
2026-03-28 19:05:04 +0300

    validates user input and shows a confirmation message

--- assignment-2/js/script.js
+++ assignment-2/js/script.js
@@ -17,3 +17,16 @@ function toggleProjects() {
     content.style.display = "none";
   }
 }
+
+function validateForm() {
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let message = document.getElementById("message").value;
+
+  if (name === "" || email === "" || message === "") {
+    alert("Please fill all fields");
+    return false;
+  } else {
+    alert("Message sent successfully!");
+  }
+}
